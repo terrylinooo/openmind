edd388f80277d60e9267730822ea6a447639436b
Terry Lin <terry@fontrip.com>
2020-03-05 23:19:50 +0800

    Ajust text.

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -6,7 +6,7 @@
          * This theme is created by Terry Lin.
          * Please keep the credit link is best support for me.
          */
-        var authorInfo = ' and <a href="https://github.com/terrylinooo/openmind" target="_blank">Openmind</a> theme by <a href="https://terryl.in">Terry L.</a>';
+        var authorInfo = ', <a href="https://github.com/terrylinooo/openmind" target="_blank">Openmind</a> theme by <a href="https://terryl.in">Terry L.</a>';
         $('#footer').append(authorInfo);
     });
 
