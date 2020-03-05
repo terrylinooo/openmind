65e995080426bf98d8fc9627b16a183d445619cc
Terry Lin <terry@fontrip.com>
2020-03-05 23:18:46 +0800

    Modify menu style.

---
+++ javascripts/theme.js
@@ -0,0 +1,13 @@
+(function($) {
+    'use strict';
+    
+    $(function() {
+        /**
+         * This theme is created by Terry Lin.
+         * Please keep the credit link is best support for me.
+         */
+        var authorInfo = ' and <a href="https://github.com/terrylinooo/openmind" target="_blank">Openmind</a> theme by <a href="https://terryl.in">Terry L.</a>';
+        $('#footer').append(authorInfo);
+    });
+
+})(jQuery);
